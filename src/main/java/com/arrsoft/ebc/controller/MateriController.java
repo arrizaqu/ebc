c94bcdc8210611b18a6b9cd350e054cf2e1f6612
arrizaqu <arrizaqu@yahoo.com>
2018-01-02 11:41:41 +0700

    ebc development

--- src/main/java/com/arrsoft/ebc/controller/MateriController.java
+++ src/main/java/com/arrsoft/ebc/controller/MateriController.java
@@ -1,15 +1,21 @@
 package com.arrsoft.ebc.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.arrsoft.ebc.model.Materi;
 
 @RestController
 @RequestMapping("/msapi/")
 public class MateriController {
 
 	@RequestMapping("/materi/{id}")
-	public String index(){
-		return "hello world!!";
+	public List<Materi> index(){
+		List<Materi> mList = new ArrayList();
+		return mList;
 	}
 }
